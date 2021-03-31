617a0cae7fc8629ec4f7b87943b4751f0c26cb54
Colton Kammes <ckammes@nd.edu>
2021-03-30 21:37:38 -0400

    lab 2 initial commit

---
+++ cse-30321-lab02/dogs_cats_birds_case02.py
@@ -0,0 +1,148 @@
+# using tf.2.1 in colab
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras import backend as K
+from tensorflow.keras.regularizers import l2
+
+# sanity check for the tf version
+print(tf.__version__)
+
+###################################################################################################
+# Define image size for network model -- all input images are scaled to this size.
+###################################################################################################   
+img_width, img_height = 150, 150
+
+###################################################################################################
+# Include pointers to training and validation data folders -- be sure to examine subfolder structure
+# --> I also define the # of training samples, validation samples, epochs, and batch size here.
+###################################################################################################   
+train_data_dir = 'Datasets/data/train'
+validation_data_dir = 'Datasets/data/validation'
+nb_train_samples = 5000
+nb_validation_samples = 1000
+epochs = 30
+batch_size = 100
+
+###################################################################################################
+# As before, this code simply organizes input data such that channels either come first or last
+# depending on the backend used (TensorFlow or Theano)
+###################################################################################################   
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+###################################################################################################
+# Define our CNN model
+###################################################################################################
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+################################################
+# INSERT YOUR CODE HERE
+################################################
+
+# 2nd layer
+model.add(Conv2D(64, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 3rd layer
+model.add(Conv2D(64, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 4th layer 
+model.add(Conv2D(64, (5, 5), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 5th layer
+model.add(Conv2D(64, (5, 5), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dense(3))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+###################################################################################################
+# To process images in respective directories, we can use the ImageDataGenerator class
+# The code provided here normalizes image data, etc.
+# The parameters will not be discussed further here, 
+#    but more information / options can be found at:  https://keras.io/preprocessing/image/ 
+###################################################################################################   
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+###################################################################################################
+# This is the augmentation configuration we will use for testing:  only rescaling
+###################################################################################################
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+###################################################################################################
+# Subsequent invocations of flow_from_directory() will use the paths to the training and validation 
+# data, and generate batches of data
+# 
+# Note that if you wanted to work with grayscale images (for example) you could simply change 
+# color_mode to ‘gray_scale’ (and the number of color channels)
+# 
+# Again, you will not need to change any parameters here, but a more detailed description of this 
+# class can also be found at the link above.
+###################################################################################################
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+###################################################################################################
+# Finally, the invocation of model.fit_generator will simply train the model on batches of data.  
+# More information can be found at:  https://keras.io/models/sequential/
+# --> However, this is just analogous to model.fit() discussed in other examples.
+###################################################################################################
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch= nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+import pandas as pd
+hist_df = pd.DataFrame(history.history) 
+hist_csv_file = 'dogs_cats_birds_model_case02_30epoch.csv'
+with open(hist_csv_file, mode='w') as f:
+    hist_df.to_csv(f)
+
+###################################################################################################
+# Here, we save the model weights and generate an image of the network...
+###################################################################################################
+model.save('dogs_cats_birds_model_case02_30epoch')
+
+plot_model(model, to_file='dogs_cats_birds_model_case02.png', show_shapes=True, show_layer_names=True)
+
---
+++ cse-30321-lab02/dogs_cats_birds_test.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+
+# sanity check for the tf version
+print(tf.__version__)
+
+model_path = 'dogs_cats_birds_model_case01_30epoch'
+model=load_model(model_path)
+#model.summary()
+
+# added this line to set image width, height, and channel order
+
+img_width, img_height = 128, 128
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (1,3, img_width, img_height)
+else:
+    input_shape = (1,img_width, img_height, 3)
+
+test_image_path = 'testdog.jpg'
+
+
+def test_image(filename):
+    img = image.load_img(filename, target_size=(img_width,img_height))  # AK: modified to be set by the variables img_weight and img_height
+    plt.imshow(img)    # display image
+    plt.show()
+
+    img_tensor = image.img_to_array(img)   # creates tensor
+    img_tensor = img_tensor.astype('float32') / 255.  # divides each element by 255 so it is between 0 and 1
+    img_tensor = img_tensor.reshape(input_shape) # size of input tensor # AK: modified to be set by the variables img_weight, img_height and channel order
+    
+    # predictions of the model
+    pred = model.predict(img_tensor)
+    print(pred) 
+
+    # Dictionary of all classes
+    labels = {0: 'birds', 1: 'cats', 2: 'dogs'}
+    predicted_class_indices=np.argmax(pred,axis=1) # find most likely prediction
+    predictions = [labels[k] for k in predicted_class_indices] # find the label of the most likely prediction
+
+    print(predictions)
+    
+
+test_image(test_image_path) # added this variable to be set up top
